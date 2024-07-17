ccadd09e265f7b897d19636a2496d971b2bbab2b
tanjiina <tanjina1042@gmail.com>
2024-07-17 14:41:04 +0600

    security issue

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,13 +4,14 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+console.log(import.meta.env.VITE_APIKEY)
 const firebaseConfig = {
-  apiKey: import.meta.env.API_APIKEY,
-  authDomain: import.meta.env.API_AUTHDOMAIN,
-  projectId: import.meta.env.API_PROJECTID,
-  storageBucket: import.meta.env.API_STORAGEBUCKET,
-  messagingSenderId: import.meta.env.API_MESSAGINGSENDERID,
-  appId: import.meta.env.API_APPID,
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
